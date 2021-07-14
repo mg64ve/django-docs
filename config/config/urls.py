af9c190447a8df2a6ff1f5457df13fbfc0300e67
marco <mg64ve@gmail.com>
2021-07-14 14:47:01 +0200

    photoapp.

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,4 +1,4 @@
-"""documents URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.contrib import admin 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', admin.site.urls),
-]
+    # Main app
+    path('', include('photoapp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- documents/docs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from docs.models import doc
-
-@admin.register(doc)
-class docAdmin(admin.ModelAdmin):
-    pass
